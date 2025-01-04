3b597a3680b291851695f7dbcf944563ead8209a
Mark <mark@remarkablemark.org>
2025-01-04 13:11:35 -0500

    refactor(components): refactor Container to TypeScript

--- src/components/Container.ts
+++ src/components/Container.ts
@@ -1,46 +1,51 @@
 import Phaser from 'phaser';
 
-import Options from '../options';
+import options from '../options';
 
 export class Container extends Phaser.GameObjects.Container {
-  constructor(scene, x, y) {
+  constructor(scene: Phaser.Scene, x: number, y: number) {
     super(scene, x, y);
     scene.add.existing(this);
-    //symbols column
+
     const symbols1 = scene.add.sprite(
       0,
       0,
       'symbols',
-      'symbols_' + this.randomBetween(0, 9) + '.png',
+      `symbols_${this.randomBetween(0, 9)}.png`,
     );
+
     const symbols2 = scene.add.sprite(
       0,
-      -Options.symbolHeight,
+      -options.symbolHeight,
       'symbols',
-      'symbols_' + this.randomBetween(0, 9) + '.png',
+      `symbols_${this.randomBetween(0, 9)}.png`,
     );
+
     const symbols3 = scene.add.sprite(
       0,
-      -Options.symbolHeight * 2,
+      -options.symbolHeight * 2,
       'symbols',
-      'symbols_' + this.randomBetween(0, 9) + '.png',
+      `symbols_${this.randomBetween(0, 9)}.png`,
     );
+
     const symbols4 = scene.add.sprite(
       0,
-      -Options.symbolHeight * 3,
+      -options.symbolHeight * 3,
       'symbols',
-      'symbols_' + this.randomBetween(0, 9) + '.png',
+      `symbols_${this.randomBetween(0, 9)}.png`,
     );
+
     const symbols5 = scene.add.sprite(
       0,
-      -Options.symbolHeight * 4,
+      -options.symbolHeight * 4,
       'symbols',
-      'symbols_' + this.randomBetween(0, 9) + '.png',
+      `symbols_${this.randomBetween(0, 9)}.png`,
     );
+
     this.add([symbols1, symbols2, symbols3, symbols4, symbols5]);
   }
 
-  randomBetween(min, max) {
+  private randomBetween(min: number, max: number) {
     return Phaser.Math.Between(min, max);
   }
 }
