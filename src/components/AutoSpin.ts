e91e49c7327913ebb4aebf80fd82dd8e26a3473e
Mark <mark@remarkablemark.org>
2025-01-04 17:19:04 -0500

    refactor(components): refactor AutoSpin to TypeScript

--- src/components/AutoSpin.ts
+++ src/components/AutoSpin.ts
@@ -1,122 +1,131 @@
-import Config from '../config';
-import Options from '../options';
+import type Phaser from 'phaser';
+
+import config from '../config';
+import options from '../options';
+import type { Game } from '../scenes';
 import { Sprite, Tween } from '.';
 
 export class AutoSpin {
-  constructor(scene) {
+  private scene: Game;
+  private buttonAuto!: Sprite;
+  private txtAutoSpin!: Phaser.GameObjects.DynamicBitmapText;
+  private txtSpeed!: Phaser.GameObjects.DynamicBitmapText;
+  private txtAuto!: Phaser.GameObjects.Text;
+  private timer!: Phaser.Time.TimerEvent;
+  private bgAuto!: Sprite;
+  private auto!: Sprite;
+  private btnPlus!: Sprite;
+  private btnMinus!: Sprite;
+  private btnPlay!: Sprite;
+  private btnExit!: Sprite;
+  private tweens!: Tween;
+
+  constructor(scene: Game) {
     this.scene = scene;
     this.autoSpin();
   }
 
-  autoSpin() {
+  private autoSpin() {
     this.buttonAuto = new Sprite(
       this.scene,
-      Config.width - 110,
-      Config.height - 50,
+      config.width - 110,
+      config.height - 50,
       'bgButtons',
       'btn-info.png',
     );
+
     this.txtAutoSpin = this.scene.add.dynamicBitmapText(
-      Config.width - 155,
-      Config.height - 70,
+      config.width - 155,
+      config.height - 70,
       'txt_bitmap',
-      Options.txtAutoSpin,
+      options.txtAutoSpin,
       38,
     );
+
     this.txtAutoSpin.setDisplayCallback(this.scene.textCallback);
+
     this.buttonAuto.on('pointerdown', () => {
-      if (!Options.checkClick) {
+      if (!options.checkClick) {
         this.buttonAuto.setScale(0.9);
-        //fuction play speed auto
         this.playSpeedAuto();
       }
     });
+
     this.buttonAuto.on('pointerup', () => this.buttonAuto.setScale(1));
   }
 
-  playSpeedAuto() {
-    //set text auto
-    if (Options.txtAutoSpin === 'STOP') {
-      Options.txtAutoSpin = 'AUTO';
-      this.txtAutoSpin.setText(Options.txtAutoSpin);
-      //remove timer event
+  private playSpeedAuto() {
+    if (options.txtAutoSpin === 'STOP') {
+      options.txtAutoSpin = 'AUTO';
+      this.txtAutoSpin.setText(options.txtAutoSpin);
+
       if (this.txtSpeed && this.timer) {
         this.txtSpeed.destroy();
         this.timer.remove();
       }
     } else {
-      Options.txtAutoSpin = 'STOP';
-      this.txtAutoSpin.setText(Options.txtAutoSpin);
-      //play audio button
+      options.txtAutoSpin = 'STOP';
+      this.txtAutoSpin.setText(options.txtAutoSpin);
       this.scene.audioPlayButton();
 
       this.bgAuto = new Sprite(
         this.scene,
-        Config.width / 2,
-        Config.height / 2,
+        config.width / 2,
+        config.height / 2,
         'autoSpin',
         'bg_auto.png',
       );
+
       this.auto = new Sprite(
         this.scene,
-        Config.width / 2,
-        Config.height / 2 - 100,
+        config.width / 2,
+        config.height / 2 - 100,
         'bgButtons',
         'btn-spin.png',
       );
 
       this.txtAuto = this.scene.add.text(
-        Config.width / 2 - 5,
-        Config.height / 2 - 115,
-        Options.txtAuto,
+        config.width / 2 - 5,
+        config.height / 2 - 115,
+        String(options.txtAuto),
         { fontSize: '35px', color: '#fff', fontFamily: 'PT Serif' },
       );
 
-      //function set text x auto
       this.setXAuto();
-
-      //function plus
       this.plus();
-
-      //function minus
       this.minus();
-
-      //function play
       this.play();
-
-      //function exit
       this.exit();
     }
   }
 
-  plus() {
+  private plus() {
     this.btnPlus = new Sprite(
       this.scene,
-      Config.width / 2 - 100,
-      Config.height / 2 - 100,
+      config.width / 2 - 100,
+      config.height / 2 - 100,
       'autoSpin',
       'btn_plus_bet.png',
     );
 
     this.btnPlus.on('pointerdown', () => {
-      //play audio button
       this.scene.audioPlayButton();
-      if (Options.txtAuto < 100) {
+
+      if (options.txtAuto < 100) {
         this.btnMinus.clearTint();
         this.btnPlus.setScale(0.9);
-        Options.txtAuto += 5;
+        options.txtAuto += 5;
 
-        //set text x auto
-        if (Options.txtAuto < 100) {
+        if (options.txtAuto < 100) {
           this.txtAuto.x = 620;
         } else {
           this.txtAuto.x = 610;
         }
 
-        this.txtAuto.setText(Options.txtAuto);
+        this.txtAuto.setText(String(options.txtAuto));
       }
 
-      if (Options.txtAuto === 100) {
+      if (options.txtAuto === 100) {
         this.btnPlus.setTint(0xa09d9d);
       }
     });
@@ -124,130 +133,135 @@ export class AutoSpin {
     this.btnPlus.on('pointerup', () => this.btnPlus.setScale(1));
   }
 
-  minus() {
+  private minus() {
     this.btnMinus = new Sprite(
       this.scene,
-      Config.width / 2 + 100,
-      Config.height / 2 - 100,
+      config.width / 2 + 100,
+      config.height / 2 - 100,
       'autoSpin',
       'btn_minus_bet.png',
     );
+
     this.btnMinus.on('pointerdown', () => {
-      //play audio button
       this.scene.audioPlayButton();
-      if (Options.txtAuto > 5) {
+
+      if (options.txtAuto > 5) {
         this.btnPlus.clearTint();
         this.btnMinus.setScale(0.9);
-        Options.txtAuto -= 5;
-        //function set text x auto
+        options.txtAuto -= 5;
         this.setXAuto();
-        this.txtAuto.setText(Options.txtAuto);
+        this.txtAuto.setText(String(options.txtAuto));
       }
-      if (Options.txtAuto === 5) {
+
+      if (options.txtAuto === 5) {
         this.btnMinus.setTint(0xa09d9d);
       }
     });
+
     this.btnMinus.on('pointerup', () => this.btnMinus.setScale(1));
   }
 
-  play() {
+  private play() {
     this.btnPlay = new Sprite(
       this.scene,
-      Config.width / 2,
-      Config.height / 2 + 100,
+      config.width / 2,
+      config.height / 2 + 100,
       'bgButtons',
       'btn_play.png',
     ).setScale(0.9);
+
     this.btnPlay.on('pointerdown', () => {
-      //play audio button
       this.scene.audioPlayButton();
-      //function remove image auto
       this.removeImgAuto();
-      if (this.scene.valueMoney >= Options.coin * Options.line)
-        this.speedPlay(Options.txtAuto);
-      else this.setTextAuto();
+
+      if (this.scene.valueMoney >= options.coin * options.line) {
+        this.speedPlay(options.txtAuto);
+      } else {
+        this.setTextAuto();
+      }
     });
   }
 
-  exit() {
+  private exit() {
     this.btnExit = new Sprite(
       this.scene,
-      Config.width - 30,
-      Config.height - 635,
+      config.width - 30,
+      config.height - 635,
       'bgButtons',
       'btn_exit.png',
     ).setScale(0.9);
+
     this.btnExit.on('pointerdown', () => {
-      //play audio button
       this.scene.audioPlayButton();
-      //function remove image auto
       this.removeImgAuto();
-      //set text auto
       this.setTextAuto();
     });
   }
 
-  speedPlay(speed) {
-    //set text speed
-    let width;
-    if (speed > 5) {
-      width = Config.width - 150;
-    } else {
-      width = Config.width - 130;
-    }
+  /**
+   * Set text speed.
+   */
+  private speedPlay(speed: number) {
+    const width = speed > 5 ? config.width - 150 : config.width - 130;
 
     this.txtSpeed = this.scene.add.dynamicBitmapText(
       width,
-      Config.height / 2 - 350,
+      config.height / 2 - 350,
       'txt_bitmap',
-      speed,
+      String(speed),
       80,
     );
+
     this.txtSpeed.setDisplayCallback(this.scene.textCallback);
+
     this.timer = this.scene.time.addEvent({
       delay: 500,
-      callback: function () {
-        //set delay
+      callback: () => {
+        // @ts-expect-error Cannot assign to 'delay' because it is a read-only property.
         this.timer.delay = 4500;
-        if (speed > 0 && this.scene.valueMoney >= Options.coin * Options.line) {
-          //set color
+
+        if (speed > 0 && this.scene.valueMoney >= options.coin * options.line) {
           this.scene.baseSpin.setColor();
-          //set check click = true
-          Options.checkClick = true;
-          //detroys line array
+          options.checkClick = true;
           this.scene.baseSpin.destroyLineArr();
-          //funtion remove text win
           this.scene.baseSpin.removeTextWin();
-          //save localStorage
           this.scene.baseSpin.saveLocalStorage();
           this.tweens = new Tween(this.scene);
           speed--;
-          this.txtSpeed.setText(speed);
+          this.txtSpeed.setText(String(speed));
         } else {
-          Options.checkClick = false;
+          options.checkClick = false;
           this.timer.remove(false);
           this.txtSpeed.destroy();
-          //set text auto
           this.setTextAuto();
         }
       },
-      callbackScope: this,
       loop: true,
     });
   }
 
-  setTextAuto() {
-    Options.txtAutoSpin = 'AUTO';
-    this.txtAutoSpin.setText(Options.txtAutoSpin);
+  private setTextAuto() {
+    options.txtAutoSpin = 'AUTO';
+    this.txtAutoSpin.setText(options.txtAutoSpin);
   }
 
-  setXAuto() {
-    if (Options.txtAuto >= 100) this.txtAuto.x = 610;
-    else if (Options.txtAuto >= 10) this.txtAuto.x = 620;
-    else this.txtAuto.x = 635;
+  private setXAuto() {
+    switch (true) {
+      case options.txtAuto >= 100:
+        this.txtAuto.x = 610;
+        break;
+
+      case options.txtAuto >= 10:
+        this.txtAuto.x = 620;
+        break;
+
+      default:
+        this.txtAuto.x = 635;
+        break;
+    }
   }
 
-  removeImgAuto() {
+  private removeImgAuto() {
     this.bgAuto.destroy();
     this.btnPlus.destroy();
     this.btnMinus.destroy();
