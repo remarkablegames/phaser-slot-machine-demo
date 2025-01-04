b21dbb944c6ce27358feb4276fa3ef49ef30a036
Mark <mark@remarkablemark.org>
2025-01-03 20:38:56 -0500

    refactor: rename base_scenes to scenes

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 import type Phaser from 'phaser';
 
-import Boot from './base_scenes/Boot';
-import Game from './base_scenes/Game';
-import Preload from './base_scenes/Preload';
+import Boot from './scenes/Boot';
+import Game from './scenes/Game';
+import Preload from './scenes/Preload';
 
 const config: Phaser.Types.Core.GameConfig = {
   width: 1280,
