ae76d05789142ec35914996a46d1d36ea5ca05b4
Mark <mark@remarkablemark.org>
2025-01-08 22:20:07 -0500

    refactor(components): render Credit inside a Container

--- src/components/Credit.tsx
+++ src/components/Credit.tsx
@@ -1,51 +1,48 @@
 import Phaser from 'phaser';
-import { createRef, Sprite, useScene } from 'phaser-jsx';
+import { Container, createRef, Sprite, useScene } from 'phaser-jsx';
 
 import config from '../config';
 import type { Game } from '../scenes';
 
 export function Credit() {
   const scene = useScene<Game>();
-  const exitRef = createRef<Phaser.GameObjects.Sprite>();
-  const paylinesRef = createRef<Phaser.GameObjects.Sprite>();
+  const closeRef = createRef<Phaser.GameObjects.Sprite>();
+  const containerRef = createRef<Phaser.GameObjects.Container>();
 
-  function toggleVisible() {
-    const visible = !paylinesRef.current?.visible;
+  function toggleModal() {
     scene.audioPlayButton();
-    paylinesRef.current?.setToTop().setVisible(visible);
-    exitRef.current?.setToTop().setVisible(visible);
+    containerRef.current?.setToTop().setVisible(!containerRef.current?.visible);
   }
 
   return (
     <>
       <Sprite
         frame="btn-credits.png"
-        onPointerDown={toggleVisible}
+        onPointerDown={toggleModal}
         scale={0.7}
         texture="about"
         x={config.width - 235}
         y={config.height - 680}
       />
 
-      <Sprite
-        frame="paylines.png"
-        ref={paylinesRef}
-        texture="about"
-        visible={false}
-        x={config.width / 2}
-        y={config.height / 2}
-      />
+      <Container visible={false} ref={containerRef}>
+        <Sprite
+          frame="paylines.png"
+          texture="about"
+          x={config.width / 2}
+          y={config.height / 2}
+        />
 
-      <Sprite
-        frame="btn_exit.png"
-        onPointerDown={toggleVisible}
-        ref={exitRef}
-        scale={0.9}
-        texture="bgButtons"
-        visible={false}
-        x={config.width - 30}
-        y={config.height - 635}
-      />
+        <Sprite
+          frame="btn_exit.png"
+          onPointerDown={toggleModal}
+          ref={closeRef}
+          scale={0.9}
+          texture="bgButtons"
+          x={config.width - 30}
+          y={config.height - 635}
+        />
+      </Container>
     </>
   );
 }
